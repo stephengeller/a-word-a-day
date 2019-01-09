7aac4b64e808c3e5cb92c28792030feae5f81b47
Stephen Geller <stephen.gb.geller+github@gmail.com>
2019-01-09 16:51:28 +0000

    Lay out structure of bot, with prettier formatting.

--- index.js
+++ index.js
@@ -1,38 +1,58 @@
 const Twit = require("twit");
-require('dotenv').config();
+require("dotenv").config();
 
 const {
-    TWITTER_CONSUMER_KEY,
-    TWITTER_CONSUMER_SECRET,
-    TWITTER_ACCESS_TOKEN,
-    TWITTER_ACCESS_TOKEN_SECRET
+  TWITTER_CONSUMER_KEY,
+  TWITTER_CONSUMER_SECRET,
+  TWITTER_ACCESS_TOKEN,
+  TWITTER_ACCESS_TOKEN_SECRET
 } = process.env;
 
 const bot = new Twit({
-    consumer_key: TWITTER_CONSUMER_KEY,
-    consumer_secret: TWITTER_CONSUMER_SECRET,
-    access_token: TWITTER_ACCESS_TOKEN,
-    access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
-    timeout_ms: 60 * 1000
+  consumer_key: TWITTER_CONSUMER_KEY,
+  consumer_secret: TWITTER_CONSUMER_SECRET,
+  access_token: TWITTER_ACCESS_TOKEN,
+  access_token_secret: TWITTER_ACCESS_TOKEN_SECRET,
+  timeout_ms: 60 * 1000
 });
 
+function getRandomNewsTitle() {
+  return "some random news title";
+}
+
+function getRandomWord(string) {
+  const wordArr = string.split(" ");
+  return wordArr[Math.floor(Math.random() * wordArr.length)];
+}
+
+function getDefinition(string) {
+  return `Definition of ${string}: ""`;
+}
+
 const postToTwitter = (message, dateAndTime) => {
-    return bot.post("statuses/update", { status: message }, (err, data) => {
-        if (err) {
-            console.log(err);
-        } else {
-            const notice = dateAndTime + ": **** " + `"${data.text}"` + " was tweeted ****"
-            console.log(notice);
-            return notice
-        }
-    });
+  return bot.post(
+    "statuses/update",
+    { status: message + " at " + dateAndTime },
+    (err, data) => {
+      if (err) {
+        console.log(err);
+      } else {
+        const notice =
+          dateAndTime + ": **** " + `"${data.text}"` + " was tweeted ****";
+        console.log(notice);
+        return notice;
+      }
+    }
+  );
 };
 
 exports.handler = function(event, context, callback) {
-    const d = new Date();
-    const time = d.toLocaleTimeString();
-    const date = d.toLocaleDateString();
-    const dateAndTime = `${date} ${time}`;
-    const string = "it is currently: " + dateAndTime;
-    postToTwitter(string, dateAndTime)
+  const d = new Date();
+  const time = d.toLocaleTimeString();
+  const date = d.toLocaleDateString();
+  const dateAndTime = `${date} ${time}`;
+  const newsTitle = getRandomNewsTitle();
+  const randomWord = getRandomWord(newsTitle);
+  const wordDefinition = getDefinition(randomWord);
+  postToTwitter(wordDefinition, dateAndTime);
 };
