0c5eb037aa686989b0ea630172fbc8ea9f5e15fc
Stephen Geller <stephen.gb.geller+github@gmail.com>
2019-01-10 11:45:53 +0000

    chore(lambda): Create deploy script

---
+++ deploy.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+set -e
+
+source ./.env
+
+cd src
+
+function lint_files() {
+    echo "Checking linting errors..."
+    eslint index.js
+}
+
+function zip_files() {
+    echo "Zipping up..."
+    zip -r ../${NAME_OF_ZIPPED_FILE} * &>/dev/null
+    cd - &>/dev/null
+}
+
+function upload_to_aws() {
+    echo "Uploading to AWS Lambda..."
+    aws lambda update-function-code --profile personal --publish --region us-east-1 --function-name ${FUNCTION_NAME} --zip-file fileb://${NAME_OF_ZIPPED_FILE}.zip
+    rm -rf ${NAME_OF_ZIPPED_FILE}.zip
+    echo "Done."
+}
+
+#lint_files
+zip_files
+upload_to_aws
